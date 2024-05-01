032cfbfef4b80f4676dc1861c8f9fe1c6a261768
Mikhail <125125622+Mikhail-07@users.noreply.github.com>
2024-05-01 19:19:45 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,12 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: 'postgres',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   }
-});
-
-  // подключение базы
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASSWORD,
-  // {
-  //   dialect: 'postgres',
-  //   host: process.env.DB_HOST,
-  //   port: process.env.DB_PORT
-  // }
+)
