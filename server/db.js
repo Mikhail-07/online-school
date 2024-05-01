a9568c881e2b3b8b1633bf0bbef7622fd1f31ca6
Mikhail Lagutik <lma3588329@gmail.com>
2024-05-01 16:23:43 +0300

    db updated

--- server/db.js
+++ server/db.js
@@ -1,12 +1,21 @@
 const {Sequelize} = require('sequelize');
+const {Pool} = require()
+
+const pool = new Pool({
+  connectionString: "postgres://default:BPawHjy1FU3Z@ep-raspy-star-a2fszb1r-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+})
 
 module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
-  }
+  // подключение базы
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  // {
+  //   dialect: 'postgres',
+  //   host: process.env.DB_HOST,
+  //   port: process.env.DB_PORT
+  // }
+
+  // подключение базы vercel
+  pool
 )
