fa7459b3f53f5d502ebf428a305061cd9140d71e
Mikhail Lagutik <lma3588329@gmail.com>
2024-10-04 11:01:10 +0300

    upd audio streaming

--- server/index.js
+++ server/index.js
@@ -31,34 +31,38 @@ app.get('/', (req, res) => {
 })
 
 app.get('/audio/:filename', (req, res) => {
-  const filePath = path.join(__dirname, 'audio', req.params.filename);
-  const stat = fs.statSync(filePath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
+  const filePath = path.join(staticDir, req.params.filename);
+  if (fs.existsSync(filePath)) {
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
 
-  if (range) {
-      const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
-      const chunkSize = (end - start) + 1;
-      const file = fs.createReadStream(filePath, { start, end });
-      const head = {
-          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunkSize,
-          'Content-Type': 'audio/mpeg',
-      };
+        const chunkSize = (end - start) + 1;
+        const file = fs.createReadStream(filePath, { start, end });
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'audio/mpeg',
+        };
 
-      res.writeHead(206, head);
-      file.pipe(res);
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'audio/mpeg',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(filePath).pipe(res);
+    }
   } else {
-      const head = {
-          'Content-Length': fileSize,
-          'Content-Type': 'audio/mpeg',
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(filePath).pipe(res);
+    res.status(404).send('File not found');
   }
 });
 
