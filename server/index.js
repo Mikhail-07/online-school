42ee6d18976c3f2ed945c27ab55675c2dd2d8186
Mikhail Lagutik <lma3588329@gmail.com>
2024-10-04 10:46:29 +0300

    Progressive Streaming audio implementation

--- server/index.js
+++ server/index.js
@@ -30,6 +30,38 @@ app.get('/', (req, res) => {
   res.status(200).json({message: 'Working!!!'})
 })
 
+app.get('/audio/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'audio', req.params.filename);
+  const stat = fs.statSync(filePath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunkSize = (end - start) + 1;
+      const file = fs.createReadStream(filePath, { start, end });
+      const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunkSize,
+          'Content-Type': 'audio/mpeg',
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+  } else {
+      const head = {
+          'Content-Length': fileSize,
+          'Content-Type': 'audio/mpeg',
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(filePath).pipe(res);
+  }
+});
+
 const start = async () => {
   try {
     sequelize.authenticate()
